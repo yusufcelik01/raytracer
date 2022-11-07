ef02e56ca9be3204dd83aa4e4e636a80089522b3
yusufcelik01 <celikyusuf2@gmail.com>
2022-11-07 09:29:17 +0300

    add bounding box class

---
+++ BoundingBox.cpp
@@ -0,0 +1,45 @@
+#include <limits>
+#include <algorithm>
+
+#include "BoundingBox.hpp"
+
+
+BoundingBox::BoundingBox()
+{
+    xmin = xmax = ymin = ymax = zmin = zmax = 
+        std::numeric_limits<float>::quiet_NaN();
+}
+
+
+BoundingBox::BoundingBox(float x_min, float x_max,
+                         float y_min, float y_max, 
+                         float z_min, float z_max)
+{
+    this->xmin = x_min; this->xmax = x_max;
+    this->ymin = y_min; this->ymax = y_max;
+    this->zmin = z_min; this->zmax = z_max;
+}
+
+bool BoundingBox::hitRay(const Ray& r)
+{
+    float tx1, tx2;
+    float ty1, ty2;
+    float tz1, tz2;
+
+    tx1 = (xmin - r.o.x)/r.d.x;
+    tx2 = (xmax - r.o.x)/r.d.x;
+    if(tx1 > tx2){std::swap(tx1, tx2);}
+     
+    ty1 = (ymin - r.o.y)/r.d.y;
+    ty2 = (ymax - r.o.y)/r.d.y;
+    if(ty1 > ty2){std::swap(ty1, ty2);}
+     
+    tz1 = (zmin - r.o.z)/r.d.z;
+    tz2 = (zmax - r.o.z)/r.d.z;
+    if(tz1 > tz2){std::swap(tz1, tz2);}
+
+    float t1 = std::max(std::max(tx1, ty1), tz1);
+    float t2 = std::max(std::max(tx2, ty2), tz2);
+
+    return !(t1 > t2);
+}
---
+++ BoundingBox.hpp
@@ -0,0 +1,23 @@
+#ifndef __BOUNDING__BOX__DEFINED
+#define __BOUNDING__BOX__DEFINED
+
+#include "rtmath.hpp"
+#include "Ray.hpp"
+
+
+class BoundingBox
+{
+    public:
+        float xmin, xmax;
+        float ymin, ymax;
+        float zmin, zmax;
+
+   BoundingBox();
+   BoundingBox(float x_min, float x_max,
+               float y_min, float y_max, 
+               float z_min, float z_max);
+   //member functions
+   bool hitRay(const Ray& r);
+};
+
+#endif
