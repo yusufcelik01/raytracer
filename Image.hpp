f3bcdf105dafd7c1ed3ad3a5d724c0640902ab1b
yusufcelik01 <celikyusuf2@gmail.com>
2022-12-04 17:04:58 +0300

    add image class

---
+++ Image.hpp
@@ -0,0 +1,12 @@
+#ifndef __IMAGE__CLASS__DEFINED__
+#define __IMAGE__CLASS__DEFINED__
+
+class Image
+{
+    public:
+        int width, height;
+        int numOfChannels;
+        unsigned char* data;    
+};
+
+#endif
