222dabd58584d324274eed5d4c238902e2993450
yusufcelik01 <celikyusuf2@gmail.com>
2022-12-12 17:21:34 +0300

    remove parser namespace

--- parser.h
+++ parser.h
@@ -17,68 +17,64 @@
 #include "Texture.hpp"
 #include "ImageTexture.hpp"
 
-namespace parser
+struct Scene
 {
+    //Data
+    vec3i background_color;
+    Texture* background_texture = NULL; 
+    float shadow_ray_epsilon;
+    int max_recursion_depth;
+    std::vector<Camera> cameras;
+    std::vector<Image> images;
+    std::vector<Texture*> textures;
 
+    vec3f ambient_light;
+    std::vector<PointLight> point_lights;
+    std::vector<AreaLight> area_lights;
 
-    struct Scene
-    {
-        //Data
-        vec3i background_color;
-        Texture* background_texture = NULL; 
-        float shadow_ray_epsilon;
-        int max_recursion_depth;
-        std::vector<Camera> cameras;
-        std::vector<Image> images;
-        std::vector<Texture*> textures;
-        
-        vec3f ambient_light;
-        std::vector<PointLight> point_lights;
-        std::vector<AreaLight> area_lights;
+    std::vector<Material> materials;
+    std::vector<mat4x4> rotations;
+    std::vector<mat4x4> scalings;
+    std::vector<mat4x4> translations;
 
-        std::vector<Material> materials;
-        std::vector<mat4x4> rotations;
-        std::vector<mat4x4> scalings;
-        std::vector<mat4x4> translations;
-        
-        //std::vector<Images> images;
-        //std::vector<Textures> textures;
+    //std::vector<Images> images;
+    //std::vector<Textures> textures;
 
-        std::vector<vec3f> vertex_data;
-        //std::vector<Sphere*> spheres;
-        //std::vector<Triangle*> triangles;
-        std::vector<Mesh*> meshes;
-        std::vector<Object*> objects;
-        Object* AccStrBVH;
+    std::vector<vec3f> vertex_data;
+    std::vector<vec2f> textureCoords;
+    //std::vector<Sphere*> spheres;
+    //std::vector<Triangle*> triangles;
+    std::vector<Mesh*> meshes;
+    std::vector<Object*> objects;
+    Object* AccStrBVH;
 
-        //Functions
-        void loadFromXml(const std::string &filepath);
-        void render(Camera camera);
-        void render(size_t cameraId);
-        bool rayQuery(Ray ray, IntersectionData& closestObjData, bool isShadowRay, float maxT);
-        bool rayQuery(Ray ray, IntersectionData& retData, bool isShadowRay, float maxT, float nearDist, vec3f gaze);
-        vec3f getRayColor(Ray ray, int depth, bool isPrimaryRay, Material currentMedium);
-        vec3f getRayColor(Ray ray, int depth, bool isPrimaryRay, Material currentMedium, vec3f gaze, float nearDist);
-        vec3f getObjNorm(const IntersectionData& data);
-        vec3f calculateLighting(Ray eyeRay, Material material, vec3f surfNorm, vec3f p);
+    //Functions
+    void loadFromXml(const std::string &filepath);
+    void render(Camera camera);
+    void render(size_t cameraId);
+    bool rayQuery(Ray ray, IntersectionData& closestObjData, bool isShadowRay, float maxT);
+    bool rayQuery(Ray ray, IntersectionData& retData, bool isShadowRay, float maxT, float nearDist, vec3f gaze);
+    vec3f getRayColor(Ray ray, int depth, bool isPrimaryRay, Material currentMedium);
+    vec3f getRayColor(Ray ray, int depth, bool isPrimaryRay, Material currentMedium, vec3f gaze, float nearDist);
+    vec3f getObjNorm(const IntersectionData& data);
+    vec3f calculateLighting(Ray eyeRay, Material material, vec3f surfNorm, vec3f p);
 
-        private:
-        void renderRow(void*);
-        void renderRowMultiSampled(void*);
+    private:
+    void renderRow(void*);
+    void renderRowMultiSampled(void*);
 
-        //parsing helpers
-        void getObjAttributes(tinyxml2::XMLNode* element, Object* obj);
-        //void parseTextures(tinyxml2::XMLNode* sceneNode);
-        void parseTextures(tinyxml2::XMLNode* sceneNode, const char* inputFileDir);
-    };
+    //parsing helpers
+    void getObjAttributes(tinyxml2::XMLNode* element, Object* obj);
+    //void parseTextures(tinyxml2::XMLNode* sceneNode);
+    void parseTextures(tinyxml2::XMLNode* sceneNode, const char* inputFileDir);
+};
 
-    struct plyData
-    {
-        std::vector<vec3f> vertices;
-        std::vector<Face> triangles;
-    };
-}
+struct plyData
+{
+    std::vector<vec3f> vertices;
+    std::vector<Face> triangles;
+};
 
-void parsePly(char* filePath, parser::plyData& plyMesh);
+void parsePly(char* filePath, plyData& plyMesh);
 
 #endif
