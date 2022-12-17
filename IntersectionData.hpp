585066d1cf9f690a1a54fcc68aeec9498c1d66d3
yusufcelik01 <celikyusuf2@gmail.com>
2022-12-17 18:19:31 +0300

    remove unnecessary lines

--- IntersectionData.hpp
+++ IntersectionData.hpp
@@ -2,6 +2,7 @@
 #define __INTERSECTION__POINT__DATA
 
 #include "Material.hpp"
+#include "Matrix.hpp"
 
 enum hitObjectType
 {
@@ -16,18 +17,13 @@ class IntersectionData //this is where i break oop and go back to C
     public:
     enum hitObjectType hitType;
     float t;
-    union
-    {
-        struct{ int v0_id, v1_id, v2_id;};//triangle indices
-        struct{ int sphereCenterId; };
-    };
 
-    
     vec3f intersectionPoint;
     int material_id;
     Material material;
     vec3f normal;
     vec2f texCoord; 
+    Matrix TBN;
 };
 
 #endif
