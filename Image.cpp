f45379a7dab0adbc5bcd21728595cb2405ad4af7
yusufcelik01 <celikyusuf2@gmail.com>
2022-12-18 01:09:44 +0300

    prevent out of bounds error caused by floats

--- Image.cpp
+++ Image.cpp
@@ -2,6 +2,8 @@
 
 vec3f Image::getRGBpixel(int x, int y)
 {
+    if (x > width) x = width -1;
+    if (y > height) y = height -1;
     vec3f color;
     color.r =  data[(width * y + x)*numOfChannels    ];
     color.g =  data[(width * y + x)*numOfChannels + 1];
