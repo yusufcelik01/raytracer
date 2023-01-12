61eb56026fc279c32233f657c6dbdd8be9ffaaca
yusufcelik01 <celikyusuf2@gmail.com>
2023-01-12 23:38:22 +0300

    fix a bug that can cause an out of bounds erros for images

--- ImageTexture.cpp
+++ ImageTexture.cpp
@@ -1,5 +1,6 @@
 #include "ImageTexture.hpp"
 #include "rtmath.hpp"
+#include <algorithm>
 
 ImageTexture::ImageTexture()
 {
@@ -19,18 +20,22 @@ vec3f ImageTexture::sampleNearest(float s, float t)
 
 vec3f ImageTexture::sampleBilinear(float s, float t)
 {
-    int x = int(img.width  * s);
-    int y = int(img.height * t);
+    int width = img.width - 1;
+    int height = img.height - 1;
+    int x = int(width  * s);
+    int y = int(height * t);
+    int x_ = std::clamp(x + 1, 0, width);
+    int y_ = std::clamp(y + 1, 0, height);
 
-    float dx = (img.width * s) - x;
-    float dy = (img.height * t) - y;
+    float dx = (width * s) - x;
+    float dy = (height * t) - y;
     
     vec3f c00, c01, c10, c11;
 
     c00 = img.getRGBpixel(x    , y    );
-    c10 = img.getRGBpixel(x + 1, y    );
-    c01 = img.getRGBpixel(x    , y + 1);
-    c11 = img.getRGBpixel(x + 1, y + 1);
+    c10 = img.getRGBpixel(x_   , y    );
+    c01 = img.getRGBpixel(x    , y_   );
+    c11 = img.getRGBpixel(x_   , y_   );
 
 
     return mix(mix(c00, c10, dx),
@@ -40,10 +45,12 @@ vec3f ImageTexture::sampleBilinear(float s, float t)
 
 vec3f ImageTexture::sample(float x, float y)
 {
-    x = x > 1.0f ? 1.0f : x;
-    y = y > 1.0f ? 1.0f : y;
-    x = x < 0.0f ? 0.0f : x;
-    y = y < 0.0f ? 0.0f : y;
+    //x = x > 1.0f ? 1.0f : x;
+    //y = y > 1.0f ? 1.0f : y;
+    //x = x < 0.0f ? 0.0f : x;
+    //y = y < 0.0f ? 0.0f : y;
+    x = std::clamp(x, 0.f ,1.f);
+    y = std::clamp(y, 0.f ,1.f);
 
     //return sampleNearest(x, y);
     vec3f sampleColor; 
