d13d8c839b8afeaf15dd80569de64308bf833557
yusufcelik01 <celikyusuf2@gmail.com>
2023-02-02 23:41:16 +0300

    add min max functions and fix typo in class name

--- src/Spectrum.hpp
+++ src/Spectrum.hpp
@@ -2,17 +2,7 @@
 #define __SPECTRUM_DEFINED__
 
 #include <cmath>
-
-//#ifdef SPECTRAL_RAY_TRACER
-//    typedef SampledSpectrum Spectrum;
-//#else 
-//    typedef RGBSpectrum Spectrum;
-//#endif
-//
-typedef RGBSpectrum Spectrum;
-
-//TODO define lambdas and sample count
-
+#include "vec3.hpp"
 
 static const int lambdaStart = 380;
 static const int lambdaEnd = 700;
@@ -21,153 +11,195 @@ static const int SpectralSampleCount = 60;
 
 enum class SpectrumType {
     Reflectance,
-    Illumination
+    Illuminant
 };
 
 
 template <int nSamples>
-class CoefficentSpectrum
+class CoefficientSpectrum
 {
     protected:
         float c[nSamples];
     public:
-        CoefficentSpectrum(float v = 0.f) {
+        CoefficientSpectrum(float v = 0.f) {
             for(int i = 0; i < nSamples; ++i){
                 c[i] = v;
             }
         }
 
-        CoefficentSpectrum(const CoefficentSpectrum& rhs) {
+        CoefficientSpectrum(const CoefficientSpectrum& rhs) {
             for(int i = 0; i < nSamples; ++i){
                 c[i] = rhs.c[i];
             }
         }
 
-        CoefficentSpectrum hasNaNs() {
+        CoefficientSpectrum hasNaNs() {
             for(int i = 0; i < nSamples; ++i){
                 if(c[i] != c[i] ) return true;
             }
         }
 
-        CoefficentSpectrum operator+(const CoefficentSpectrum& rhs) const{
-            CoefficentSpectrum tmp;
+        float minCoefVal() {
+            float min = c[0];
+            for(int i = 1; i < nSamples; ++i){
+                min = (min < c[i]) ? min : c[i];
+            }
+            return min;
+        }
+
+        float maxCoefVal() {
+            float max = c[0];
+            for(int i = 1; i < nSamples; ++i){
+                max = (max > c[i]) ? max : c[i];
+            }
+            return max;
+        }
+
+        CoefficientSpectrum operator+(const CoefficientSpectrum& rhs) const{
+            CoefficientSpectrum tmp;
             for(int i = 0; i < nSamples; ++i){
                 tmp.c[i] = c[i] + rhs.c[i];
             }
             return tmp;
         }
 
-        CoefficentSpectrum operator-(const CoefficentSpectrum& rhs) const{
-            CoefficentSpectrum tmp;
+        CoefficientSpectrum operator-(const CoefficientSpectrum& rhs) const{
+            CoefficientSpectrum tmp;
             for(int i = 0; i < nSamples; ++i){
                 tmp.c[i] = c[i] - rhs.c[i];
             }
             return tmp;
         }
 
-        CoefficentSpectrum operator*(const CoefficentSpectrum& rhs) const{
-            CoefficentSpectrum tmp;
+        CoefficientSpectrum operator*(const CoefficientSpectrum& rhs) const{
+            CoefficientSpectrum tmp;
             for(int i = 0; i < nSamples; ++i){
                 tmp.c[i] = c[i] * rhs.c[i];
             }
             return tmp;
         }
 
-        CoefficentSpectrum operator/(const CoefficentSpectrum& rhs) const{
-            CoefficentSpectrum tmp;
+        CoefficientSpectrum operator*(const float& s) const{
+            CoefficientSpectrum tmp;
+            for(int i = 0; i < nSamples; ++i){
+                tmp.c[i] = c[i] * s;
+            }
+            return tmp;
+        }
+
+        CoefficientSpectrum operator/(const float& s) const{
+            CoefficientSpectrum tmp;
+            for(int i = 0; i < nSamples; ++i){
+                tmp.c[i] = c[i] / s;
+            }
+            return tmp;
+        }
+
+        CoefficientSpectrum operator/(const CoefficientSpectrum& rhs) const{
+            CoefficientSpectrum tmp;
             for(int i = 0; i < nSamples; ++i){
                 tmp.c[i] = c[i] / rhs.c[i];
             }
             return tmp;
         }
 
-        CoefficentSpectrum operator-() const{
-            CoefficentSpectrum tmp;
+        CoefficientSpectrum operator-() const{
+            CoefficientSpectrum tmp;
             for(int i = 0; i < nSamples; ++i){
                 tmp.c[i] = -c[i];
             }
             return tmp;
         }
         //assignment ops
-        CoefficentSpectrum& operator=(const CoefficentSpectrum& rhs) const{
+        CoefficientSpectrum& operator=(const CoefficientSpectrum& rhs){
             for(int i = 0; i < nSamples; ++i){
                 c[i] = rhs.c[i];
             }
             return *this;
         }
 
-        CoefficentSpectrum& operator+=(const CoefficentSpectrum& rhs) const{
+        CoefficientSpectrum& operator+=(const CoefficientSpectrum& rhs) {
             for(int i = 0; i < nSamples; ++i){
                 c[i] += rhs.c[i];
             }
             return *this;
         }
 
-        CoefficentSpectrum& operator-=(const CoefficentSpectrum& rhs) const{
+        CoefficientSpectrum& operator-=(const CoefficientSpectrum& rhs) {
             for(int i = 0; i < nSamples; ++i){
                 c[i] -= rhs.c[i];
             }
             return *this;
         }
 
-        CoefficentSpectrum& operator*=(const CoefficentSpectrum& rhs) const{
+        CoefficientSpectrum& operator*=(const CoefficientSpectrum& rhs) {
             for(int i = 0; i < nSamples; ++i){
                 c[i] *= rhs.c[i];
             }
             return *this;
         }
 
-        CoefficentSpectrum& operator/=(const CoefficentSpectrum& rhs) const{
+        CoefficientSpectrum& operator/=(const CoefficientSpectrum& rhs) {
             for(int i = 0; i < nSamples; ++i){
                 c[i] /= rhs.c[i];
             }
             return *this;
         }
 
-        friend CoefficentSpectrum Sqrt(const CoefficentSpectrum& rhs) {
-            CoefficentSpectrum tmp;
+        friend CoefficientSpectrum Sqrt(const CoefficientSpectrum& rhs) {
+            CoefficientSpectrum tmp;
             for(int i = 0; i < nSamples; ++i){
                 tmp.c[i] = std::sqrt(rhs.c[i]);
             }
             return tmp;
         }
 
-        friend CoefficentSpectrum Exp(const CoefficentSpectrum& rhs) {
-            CoefficentSpectrum tmp;
+        friend CoefficientSpectrum Exp(const CoefficientSpectrum& rhs) {
+            CoefficientSpectrum tmp;
             for(int i = 0; i < nSamples; ++i){
                 tmp.c[i] = std::exp(rhs.c[i]);
             }
             return tmp;
         }
 
-        friend CoefficentSpectrum Pow(const CoefficentSpectrum& rhs, float n) {
-            CoefficentSpectrum tmp;
+        friend CoefficientSpectrum Pow(const CoefficientSpectrum& rhs, float n) {
+            CoefficientSpectrum tmp;
             for(int i = 0; i < nSamples; ++i){
                 tmp.c[i] = std::pow(rhs.c[i], n);
             }
             return tmp;
         }
 
-        friend CoefficentSpectrum Lerp(float a, 
-                                       const CoefficentSpectrum& s1, 
-                                       const CoefficentSpectrum& s2) {
-            CoefficentSpectrum tmp;
+        friend CoefficientSpectrum Lerp(float a, 
+                                       const CoefficientSpectrum& s1, 
+                                       const CoefficientSpectrum& s2) {
+            CoefficientSpectrum tmp;
             for(int i = 0; i < nSamples; ++i){
                 tmp.c[i] = (1.f - a) * s1.c[i] + a * s2.c[i];
             }
             return tmp;
         }
 
+        friend CoefficientSpectrum operator*(const float& c, const CoefficientSpectrum& v) {
+            CoefficientSpectrum ret(v);
+            for(int i = 0; i < nSamples; ++i) {
+                ret.c[i] *= c;
+            }
+            return ret;
+        }
    
 };
 
-class RGBSpectrum : public CoefficentSpectrum<3> {
+
+
+class RGBSpectrum : public CoefficientSpectrum<3> {
     //fromRGB(float r, float g, float b); 
     
-    RGBSpectrum(float v = 0.f) : CoefficentSpectrum<3>(v) {}
-    RGBSpectrum(const CoefficentSpectrum<3> &v) : CoefficentSpectrum<3>(v) {}
+    public:
+    RGBSpectrum(float v = 0.f) : CoefficientSpectrum<3>(v) {}
+    RGBSpectrum(const CoefficientSpectrum<3> &v) : CoefficientSpectrum<3>(v) {}
     
-    static RGBSpectrum fromRGB(float rgb[3]) {
+    static RGBSpectrum fromRGB(vec3f rgb, SpectrumType type = SpectrumType::Reflectance) {
         RGBSpectrum s;
         s.c[0] = rgb[0];
         s.c[1] = rgb[1];
@@ -175,30 +207,41 @@ class RGBSpectrum : public CoefficentSpectrum<3> {
         return s;
     }
 
-    void toRGB(float rgb[3]) const {
+    vec3f toRGB() const {
+        vec3f rgb;
         rgb[0] = c[0]; 
         rgb[1] = c[1]; 
         rgb[2] = c[2]; 
+        return rgb;
     }
 
-    static RGBSpectrum fromXYZ(float xyz[3]) const {
+    static RGBSpectrum fromXYZ(vec3f xyz) {
         RGBSpectrum s;
-        s.c[0] = r;
-        s.c[1] = g;
-        s.c[2] = b;
+        s.c[0] = xyz[0];
+        s.c[1] = xyz[1];
+        s.c[2] = xyz[2];
         return s;
     }
 
-    
-    void RGBSpectrum toXYZ(float xyz[3]) const {//TODO this actually not converting
+    vec3f toXYZ() const {//TODO this actually not converting
+        vec3f xyz;
         xyz[0] = c[0]; 
         xyz[1] = c[1]; 
         xyz[2] = c[2]; 
+        return xyz;
     }
-    //static RGBSpectrum fromSampled(); TODO
-     
+
 
 };
+
+
+//#ifdef SPECTRAL_RAY_TRACER
+//    typedef SampledSpectrum Spectrum;
+//#else 
+//    typedef RGBSpectrum Spectrum;
+//#endif
+//
+typedef RGBSpectrum Spectrum;
 
 
 #endif
